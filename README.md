1aab0302a40411b81ec14ecec3fc6784072172fe
Amador Criado <amatore@gmail.com>
2024-10-22 10:13:44 +0200

    Initial commit - implemented streamlit chat app and amazon bedrock chat lib.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # amazon-bedrock-rag-chatbot
 Streamlit RAG Chatbot by using Amazon bedrock converse API 
+
+
+
+## Python
+
+- create the ".venv" environment:
+
+```
+python3 -m venv .venv
+```
+
+- Activate virtual environment
+```
+source .venv/bin/activate
+```
+- Upgrade pip (recommended)
+```
+python3 -m pip install --upgrade pip
+```
+
+- Install required packages
+```
+pip install chromadb
+```
+
+- Deactivate virtual environment
+```
+deactivate
+```
+
+## Streamlit
+
+- Go to `./chatbot/` folder
+
+- You can run streamlit locally with the following 2 commands:
+
+```
+python -m streamlit run streamlit_hello_world.py
+streamlit run rag_chatbot_app.py --server.port 8080
+```
+
+
---
+++ lab/chroma_hello_world.py
@@ -0,0 +1,28 @@
+import chromadb
+import logging
+
+
+
+print("Hello, world!")
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="my_collection")
+# Add logging to print the input data
+documents = [
+    "This is a document about pineapple",
+    "This is a document about oranges"
+]
+logging.debug(f"Documents to be added: {documents}")
+
+try:
+    collection.add(
+        documents=documents,
+        ids=["id1", "id2"]
+    )
+except Exception as e:
+    logging.error(f"Error adding documents to collection: {e}")
+
+results = collection.query(
+    query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
+    n_results=2 # how many results to return
+)
+print(results)
